7d3f60d1b8b6406f794df5ec897ca12c6a1f51de
Yajat Prabhakar <yajatprabhakar@gmail.com>
2025-09-11 17:02:45 +0000

    make the gradient background match the color schema of the website

--- src/components/background-gradient.tsx
+++ src/components/background-gradient.tsx
@@ -5,8 +5,8 @@ export function BackgroundGradient() {
       className="fixed inset-0 -z-50 h-full w-full"
     >
       <div className="relative h-full w-full bg-background">
-        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,hsla(217,91%,59%,0.15),rgba(255,255,255,0))]"></div>
+        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,hsla(217,91%,59%,0.15),rgba(255,255,255,0))]"></div>
       </div>
     </div>
   );
